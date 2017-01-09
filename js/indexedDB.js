d4eb5d8f8d47f0c44923fdd77c25666fc43f2960
270953 <robert.nehring@stud.fh-luebeck.de>
2017-01-09 18:43:04 +0100

    indexedDB.js kommentiert

--- js/indexedDB.js
+++ js/indexedDB.js
@@ -2,42 +2,37 @@
 
 var datenbank;
 var objectStore;
-var ausgabeFeld = document.getElementsByTagName('output');
+var ausgabeFeld = document.getElementsByTagName('output');      // im output-Bereich werden später die letzten Ergebnisse angezeigt
 
 
 // Funktion, um die Datenbank anzulegen oder zu öffnen
 function datenbankOeffnen() {
 
     //noinspection JSUnresolvedVariable
-    var request = window.indexedDB.open("gruppe5Datenbank.db",1);
-    console.log('Er möchte eine Datenbank öffnen.');
+    var request = window.indexedDB.open("gruppe5Datenbank.db",1);       // Es wird ein request gestartet, der die Datenbank 'gruppe5Datenbank' öffnen möchte
 
-    request.onsuccess = function () {
+    request.onsuccess = function () {                                   // war der request erfolgreich und die Datenbank konnte geöffnet werden, wird das Datenbank-Objekt an die Variable 'datenbank' übergeben
         datenbank = this.result;
         console.log('Die Datenbank wurde erfolgreich geladen.');
     };
-
-    request.onerror = function () {
-        console.log('Beim Laden der Datenbank ist ein Fehler aufgetreten.');
-    };
-
-    request.onupgradeneeded = function () {
-        datenbank = this.result;
+    
+    request.onupgradeneeded = function () {                             // konnte die Datenbank nicht gefunden werden, wird eine neue Datenbank angelegt
+        datenbank = this.result;                                        // auch in diesem Fall wird das Datenbank-Objekt an die Variable 'datenbank' übergeben
         console.log('Neue Datenbank angelegt.');
 
             //noinspection JSUnresolvedVariable
-            if (!datenbank.objectStoreNames.contains('letzteBerechnungen')) {
+            if (!datenbank.objectStoreNames.contains('letztePreisBerechnungen')) {       // gibt es keinen Objectstore 'letzte Berechnungen', wird dieser angelegt
 
                 console.log('Objektstore wird angelegt.');
                 //noinspection JSUnresolvedFunction
-                datenbank.createObjectStore('letzteBerechnungen', {
-                    keyPath: 'id',
-                    autoIncrement: true
+                datenbank.createObjectStore('letztePreisBerechnungen', {
+                    keyPath: 'id',                                                  // als key wird 'id' festgelegt
+                    autoIncrement: true                                             // der key zählt bei jedem neuen Eintrag in den Objectstore automatisch einen weiter
                 });
             }
 
             //noinspection JSUnresolvedVariable
-            if (!datenbank.objectStoreNames.contains('quizErgebnisse')) {
+            if (!datenbank.objectStoreNames.contains('quizErgebnisse')) {           // gleicher Ablauf wie beim o.g. Objectstore
 
                 console.log('Objektstore wird angelegt.');
                 //noinspection JSUnresolvedFunction
@@ -46,23 +41,28 @@ function datenbankOeffnen() {
                     autoIncrement: true
                 });
             }
-        };
+    };
+
+
+    request.onerror = function () {         // War der request nicht erfolgreich und es konnte weder eine Datenbank geöffnet noch neu angelegt werden, wird eine Fehlermeldung ausgegeben
+        console.log('Beim Laden der Datenbank ist ein Fehler aufgetreten.');
+    };
 }
 
 
 // Funktion, um Daten in die Datenbank zu schreiben
-function datenSpeichern(eingabeDaten, herkunft) {
+function datenSpeichern(eingabeDaten, herkunft) {           // es werden zum einen die Daten, die gespeichert werden sollen, übergeben. zum anderen aber auch eine Prüfvariable
 
-    if (herkunft == 'preis') {
-        objectStore = datenbank.transaction(['letzteBerechnungen'], 'readwrite').objectStore('letzteBerechnungen');
+    if (herkunft == 'preis') {                              // ist die Prüfvariable = 'preis', wird eine Transaction für den Objectstore 'letztePreisBerechnungen' geöffnet
+        objectStore = datenbank.transaction(['letztePreisBerechnungen'], 'readwrite').objectStore('letztePreisBerechnungen');
     }
-    else if (herkunft == 'quiz') {
+    else if (herkunft == 'quiz') {                          // ist die Prüfvariable = 'quiz', wird eine Transaction für den Objectstore 'quizErgebnisse' geöffnet
         objectStore = datenbank.transaction(['quizErgebnisse'], 'readwrite').objectStore('quizErgebnisse');
     }
 
-    var request = objectStore.add(eingabeDaten);
+    var request = objectStore.add(eingabeDaten);            // es wird ein request mit dem Ziel gestartet, die übergebenen Daten im Objectstore zu speichern
     request.onsuccess = function (event) {
-        console.log('Eintrag ' + event.target.result + ' gespeichert.');
+        console.log('Eintrag ' + event.target.result + ' gespeichert.');    // war der request erfolgreich, erfolgt ein Konsoleneintrag
     };
 }
 
@@ -70,27 +70,29 @@ function datenSpeichern(eingabeDaten, herkunft) {
 // Funktion, um die Datenbank auszulesen
 function datenLesen(herkunft) {
 
-    ausgabeFeld[0].innerHTML = 'Hier sehen Sie die letzten Ergebnisse:<br>'; //löscht den Inhalt des Ausgabefeldes bei jedem Aufruf, damit die Liste sich nicht wiederholt
+    ausgabeFeld[0].innerHTML = 'Hier sehen Sie die letzten Ergebnisse:<br>';    // löscht gleichzeitig den Inhalt des Ausgabefeldes bei jedem Aufruf, damit die Liste sich nicht wiederholt
 
-    if (herkunft == 'preis') {
-        objectStore = datenbank.transaction(['letzteBerechnungen'], 'readwrite').objectStore('letzteBerechnungen');
+    if (herkunft == 'preis') {                              // ist die Prüfvariable = 'preis', wird eine Transaction für den Objectstore 'letztePreisBerechnungen' geöffnet
+        objectStore = datenbank.transaction(['letztePreisBerechnungen'], 'readwrite').objectStore('letztePreisBerechnungen');
     }
-    else if (herkunft == 'quiz') {
+    else if (herkunft == 'quiz') {                          // ist die Prüfvariable = 'quiz', wird eine Transaction für den Objectstore 'quizErgebnisse' geöffnet
         objectStore = datenbank.transaction(['quizErgebnisse'], 'readwrite').objectStore('quizErgebnisse');
     }
 
-    //noinspection JSUnresolvedFunction
-    objectStore.openCursor().onsuccess = function (event) {
-        var jsObjekt = event.target.result;
+    var abfrageBegrenzung = IDBKeyRange.upperBound(10);     // damit wird eine Obergrenze festgelegt, die bezweckt, dass nur die ersten zehn Einträge des Objectstores ausgelesen werden
 
-        if (jsObjekt) {
-            inHTMLwiedergeben(jsObjekt);
+    //noinspection JSUnresolvedFunction
+    objectStore.openCursor(abfrageBegrenzung).onsuccess = function (event) {        // auf dem Objectstore wird ein Zeiger geöffnet, der den Store durchläuft. Wenn ein Eintrag gefunden wird, wird das onsuccess-Event ausgelöst
+        var jsObjekt = event.target.result;                                         // findet er einen Eintrag, übergibt er das Ergebnis an 'jsObjekt'
+
+        if (jsObjekt) {                                                             // da auch undefined übergeben werden kann, erfolgt sicherheitshalber eine Abfrage
+            inHTMLwiedergeben(jsObjekt);                                            // wenn positiv, dann wird die Funktion 'inHTMLwiedergeben' aufgerufen
             //noinspection JSUnresolvedFunction
-            jsObjekt.continue();
+            jsObjekt.continue();                                                    // hiermit wird der Zeiger aufgefordert, zum nächsten Eintrag zu springen
         }
 
         else {
-            console.log("No more entries!");
+            console.log("Keine weiteren Einträge vorhanden!");                      // findet der Zeiger keinen Eintrag, erfolgt ein Konsoleneintrag
         }
     };
 
@@ -98,27 +100,23 @@ function datenLesen(herkunft) {
 
 
 // Ausgabe der gelesenen Daten in HTML
-        function inHTMLwiedergeben(jsObjekt) {
+function inHTMLwiedergeben(jsObjekt) {
 
-            ausgabeFeld[0].innerHTML += '<br>';
+    ausgabeFeld[0].innerHTML += '<br>';             // erzeugt eine Freizeile zwischen den Einträgen
 
-            if (jsObjekt) {
+    console.log(jsObjekt);                          // Ausgabe des Zeiger-Objektes in der Konsole
+    jsObjekt = jsObjekt.value;                      // da das Zeiger-Ergebnis nur auf den Eintrag in der Datenbank zeigt, dessen Daten aber noch nicht enthält, muss darauf über die Eigenschaft 'value' zugegriffen werden
+    console.log(jsObjekt);                          // Ausgabe des in der Datenbank gespeicherten Javascript-Objektes in der Konsole
 
-                console.log(jsObjekt);                          // Ausgabe des JavaScript Objektes in der Konsole
-                jsObjekt = jsObjekt.value;
-                console.log(jsObjekt);
+    for (var eigenschaft in jsObjekt) {             // iteriert durch das Objekt
+        if (jsObjekt.hasOwnProperty(eigenschaft)) { // Fehler abfangen
 
-                for (var eigenschaft in jsObjekt) {             // iteriert durch das Objekt
-                    if (jsObjekt.hasOwnProperty(eigenschaft)) {
+            if (eigenschaft != 'id') {              /* da die Datenbank jedem gespeicherten Objekt den keyPath als Eigenschaft anhängt, muss sichergestellt werden,
+                                                    dass dieser nicht mit ausgegeben wird */
+                ausgabeFeld[0].innerHTML += eigenschaft + ': ' + jsObjekt[eigenschaft] + '<br>';        // dem Ausgabefeld wird die aktuelle Eigenschaft des Objektes hinzugefügt
+            }
 
-                        if (eigenschaft != 'id') {
-
-                            ausgabeFeld[0].innerHTML += eigenschaft + ': ' + jsObjekt[eigenschaft] + '<br>';
-                        }
-
-                    }
-                }
-
+        }
     }
 
 }
